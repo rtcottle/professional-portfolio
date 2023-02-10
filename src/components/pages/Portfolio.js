3e0256e09f52945c3902c1551a5873f39aff57be
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-02-10 16:43:02 -0700

    added links for portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,16 +6,38 @@ export default function Blog() {
       <h1>Portfolio</h1>
       <div class="container">
         <div class="row justify-content-around">
-          <div class="col-md card">Column</div>
-          <div class="col-md card">Column</div>
+          <div class="col-md card">
+            <a href="https://github.com/rtcottle/notes">Notes App</a>
+          </div>
+          <div class="col-md card">
+            <a href="https://github.com/rtcottle/Park-Hopper">
+              Park Hopper App
+            </a>
+          </div>
         </div>
         <div class="row">
-          <div class="col-md card">Column</div>
-          <div class="col-md card">Column</div>
+          <div class="col-md card">
+            <a href="https://github.com/rtcottle/readme-generator">
+              README generator
+            </a>
+          </div>
+          <div class="col-md card">
+            <a href="https://github.com/rtcottle/text-editor-pwa">
+              Text Editor PWA
+            </a>
+          </div>
         </div>
         <div class="row">
-          <div class="col-md card">Column</div>
-          <div class="col-md card">Column</div>
+          <div class="col-md card">
+            <a href="https://github.com/rtcottle/StockArticleComparison">
+              Stock Article Comparison App
+            </a>
+          </div>
+          <div class="col-md card">
+            <a href="https://github.com/rtcottle/ecommerce-backend">
+              E-commerce Backend App
+            </a>
+          </div>
         </div>
       </div>
     </div>
