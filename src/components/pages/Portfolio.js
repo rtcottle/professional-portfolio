1cdb2e864fff08085587f2c5d49f1dc64bdfe2e8
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-02-11 14:01:56 -0700

    added some notes for images.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import Ecommerce from '../../images/e-commerce.png';
+import Notes from '../../images/note-app.png';
+import Park from '../../images/parkhopper.png';
+import Readme from '../../images/readme.png';
+import Stock from '../../images/stock-app.png';
+import Text from '../../images/text-editor.png';
 
 export default function Blog() {
   return (
@@ -6,34 +12,36 @@ export default function Blog() {
       <h1>Portfolio</h1>
       <div class="container">
         <div class="row justify-content-around">
-          <div class="col-md card">
-            <a href="https://github.com/rtcottle/notes">Notes App</a>
+          <div class="col-md card m-2">
+            <a href="https://github.com/rtcottle/notes">
+              <img src={Notes}></img>Notes App
+            </a>
           </div>
-          <div class="col-md card">
+          <div class="col-md card m-2">
             <a href="https://github.com/rtcottle/Park-Hopper">
               Park Hopper App
             </a>
           </div>
         </div>
         <div class="row">
-          <div class="col-md card">
+          <div class="col-md card m-2">
             <a href="https://github.com/rtcottle/readme-generator">
               README generator
             </a>
           </div>
-          <div class="col-md card">
+          <div class="col-md card m-2">
             <a href="https://github.com/rtcottle/text-editor-pwa">
               Text Editor PWA
             </a>
           </div>
         </div>
         <div class="row">
-          <div class="col-md card">
+          <div class="col-md card m-2">
             <a href="https://github.com/rtcottle/StockArticleComparison">
               Stock Article Comparison App
             </a>
           </div>
-          <div class="col-md card">
+          <div class="col-md card m-2">
             <a href="https://github.com/rtcottle/ecommerce-backend">
               E-commerce Backend App
             </a>
