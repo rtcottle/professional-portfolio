ac76de97d54b3abd1486b540b3d6fd01e0a982a2
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-03-05 10:25:46 -0700

    added header for name and email

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header class="p-1 text-center">
+      <h1 class="mb-0" id="name">
+        Ryan Cottle
+      </h1>
+      <a href="mailto:cottlert@gmail.com" id="email">
+        cottlert@gmail.com
+      </a>
+    </header>
+  );
+}
