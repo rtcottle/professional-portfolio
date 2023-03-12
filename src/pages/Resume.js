aa5b743850b851c0d23a04ecba1199f35f91eb7e
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-03-12 15:10:33 -0600

    added react pdf dependency

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
 
 export default function Resume() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
-    <div className="px-3">
-      <h2>Resume</h2>
+    <div>
+      <div className="px-3">
+        <h2>Resume</h2>
+      </div>
+      <div>
+        <Document
+          file="../images/resume.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
     </div>
   );
 }
