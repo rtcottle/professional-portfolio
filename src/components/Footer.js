6f73d91260a55c7093ecf00bf7fd015cd1d28c41
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-03-14 16:13:24 -0600

    corrected class names

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,12 @@ const LINKEDIN = process.env.REACT_APP_LINKEDIN;
 
 export default function Footer() {
   return (
-    <footer class="fixed-bottom p-3 text-end">
+    <footer className="fixed-bottom p-3 text-end">
       <a href={GITHUB}>
-        <img src={Github} alt="GitHub" class="icon m-3" />
+        <img src={Github} alt="GitHub" className="icon m-3" />
       </a>
       <a href={LINKEDIN}>
-        <img src={Linkedin} alt="LinkedIn" class="icon m-3" />
+        <img src={Linkedin} alt="LinkedIn" className="icon m-3" />
       </a>
     </footer>
   );
