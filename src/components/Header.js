aab1c9291201db4a2b6813a7c47f7a0689f14a53
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-03-16 16:44:48 -0600

    changed style and made name a return button

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="p-1 text-center">
-      <h1 className="mb-0" id="name">
-        Ryan Cottle
+      <h1 className="mb-0">
+        <a id="name" href="/" alt="About Ryan Cottle">
+          Ryan Cottle
+        </a>
       </h1>
       <a href="mailto:cottlert@gmail.com" id="email">
         cottlert@gmail.com
